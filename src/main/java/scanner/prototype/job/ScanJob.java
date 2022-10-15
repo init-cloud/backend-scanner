0ff280e0febd7254e1200e7898cdabf9a421816b
floodnut <gsniper777@gmail.com>
2022-10-15 20:59:56 +0900

    [22.10.15][add] load file with hcl parser

--- src/main/java/scanner/prototype/job/ScanJob.java
+++ src/main/java/scanner/prototype/job/ScanJob.java
@@ -4,21 +4,23 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 
+import scanner.prototype.env.Env;
+import scanner.prototype.exception.ExceptionMessage;
+import scanner.prototype.middleware.FileLoader;
+
 
 public class ScanJob {
 
-    private final static String SHELL_COMMAND = ;
-    private final static String FILE_UPLOAD_PATH = ;
-    private final static String ERROR_STRING = "ERROR";
-
     public String terrformScan(String args) 
-    throws Exception {
+    throws Exception 
+    {
+        FileLoader fl = new FileLoader();
         String scanResult;
         StringBuilder sb = new StringBuilder();
         Process p;
 
         try {
-            String[] cmd = {"/bin/bash","-c", SHELL_COMMAND};
+            String[] cmd = {"/bin/bash", "-c", Env.SHELL_COMMAND.getValue()};
             p = Runtime.getRuntime().exec(cmd);
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             while ((scanResult = br.readLine()) != null){
@@ -30,12 +32,13 @@ public class ScanJob {
             //System.out.println("exit: " + p.exitValue());
             p.destroy();
 
-            File file = new File(FILE_UPLOAD_PATH + File.separator + args);
+            File file = fl.loadTerraformFile(args); 
+            // new File(FILE_UPLOAD_PATH + File.separator + args);
             boolean result = file.delete();
 
             return sb.toString();
         } catch (Exception e) {
-            return ERROR_STRING;
+            return ExceptionMessage.ERROR.getExceptionMessage();
         }
     }
 }
