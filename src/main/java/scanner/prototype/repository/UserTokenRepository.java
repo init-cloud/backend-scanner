074179a1d409d96d1614add17f809efbdc201a0b
floodnut <gsniper777@gmail.com>
2022-11-22 16:25:33 +0900

    [22.11.22][remove] remove cache, user

--- src/main/java/scanner/prototype/repository/UserTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package scanner.prototype.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import scanner.prototype.model.User;
-
-public interface UserTokenRepository extends JpaRepository<User, String> {
-    
-}
