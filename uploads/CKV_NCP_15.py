79a6979a527b44f73c9739b0b42dd9626c2f1f78
floodnut <gsniper777@gmail.com>
2022-11-24 17:12:20 +0900

    [22.11.24][add] feat: custom scan testcode

---
+++ uploads/CKV_NCP_15.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class LBTargetGroupUsingHTTPS(BaseResourceCheck):
+
+    def __init__(self):
+        protocol = "HTTP"
+        name = f"Ensure Load Balancer Target Group is not using {protocol}"
+        id = "CKV_NCP_15_CUSTOM"
+        supported_resources = ("ncloud_lb_target_group",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        if "protocol" in conf.keys():
+            if conf.get("protocol") != ['HTTP']:
+                return CheckResult.PASSED
+        return CheckResult.FAILED
+
+
+check = LBTargetGroupUsingHTTPS()
