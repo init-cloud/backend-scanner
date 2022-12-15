9565b0387a24bce29814188dc75e7a4adb9d7519
floodnut <gsniper777@gmail.com>
2022-12-15 16:17:31 +0900

    [22.12.12] feat: report failed data.

--- src/main/java/scanner/prototype/dto/history/report/FailedComplianceDto.java
+++ src/main/java/scanner/prototype/dto/history/report/FailedComplianceDto.java
@@ -1,25 +1,41 @@
 package scanner.prototype.dto.history.report;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.LinkedHashMap;
+import java.util.List;
+import lombok.experimental.SuperBuilder;
 import scanner.prototype.model.Compliance;
+import scanner.prototype.model.ScanHistoryDetail;
 
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class FailedComplianceDto {
-    private String name;
-    private Integer count;
-
-    public static FailedComplianceDto toDto(Compliance compliance){
-
-        return FailedComplianceDto.builder()
-                                .name("-")
-                                .count(0)
-                                .build();
+@SuperBuilder
+public class FailedComplianceDto extends FailedDto{
         
+    public FailedComplianceDto(String name, Integer count) {
+        super(name, count);
+    }
+
+    public static LinkedHashMap<String, Integer> toMapDto(List<ScanHistoryDetail> details){
+
+        LinkedHashMap<String, Integer> compliance = new LinkedHashMap<>();
+
+        if(details.isEmpty())
+            return compliance;
+
+        details.stream()
+                .forEach(
+                    detail -> {
+                        List<Compliance> compliances = detail.getRuleSeq().getCompliance();
+
+                        for(int i = 0 ; i < compliances.size() ; i++){
+                            String key = compliances.get(i).getComplianceNumber();
+
+                            if(compliance.containsKey(key))
+                                compliance.put(key, compliance.get(key) + 1);
+                            else
+                                compliance.put(key, 1);
+                        }
+                    }
+                );
+
+        return compliance;
     }
 }
--- src/main/java/scanner/prototype/dto/history/report/FailedResourceDto.java
+++ src/main/java/scanner/prototype/dto/history/report/FailedResourceDto.java
@@ -1,26 +1,33 @@
 package scanner.prototype.dto.history.report;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-
-import lombok.Data;
+import lombok.experimental.SuperBuilder;
 import scanner.prototype.model.ScanHistoryDetail;
 
-@Data
-public class FailedResourceDto {
+@SuperBuilder
+public class FailedResourceDto extends FailedDto{
 
-    public static Map<String, Integer> toDto(List<ScanHistoryDetail> details){
+    public static LinkedHashMap<String, Integer> toMapDto(List<ScanHistoryDetail> details){
 
-        Map<String, Integer> resource = new HashMap<>();
+        LinkedHashMap<String, Integer> resource = new LinkedHashMap<>();
 
-        for(int i = 0 ; i < details.size() ; i++){
-            String key = details.get(i).getResource();
-            if(resource.containsKey(details.get(i).getResource()))
-                resource.put(key, resource.get(key) + 1);
-            else
-                resource.put(key, 1);
-        }
+        if(details.isEmpty())
+            return resource;
+
+        details.stream()
+                .forEach(
+                    detail ->{
+                        String key = detail.getResource();
+                        if(resource.containsKey(detail.getResource())){
+                            resource.put(key, resource.get(key) + 1);
+                        }
+
+                        else{
+                            resource.put(key, 1);
+                        }
+                    }
+                );
 
         return resource;
     }
