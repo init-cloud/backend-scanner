a76403ae0f0c00ddd1ea50c47c9212b2c0445ef7
Floodnut <gsniper777@gmail.com>
2023-01-08 18:15:08 +0900

    feat: User and UserToken entity (#27)

---
+++ src/main/java/scanner/model/User.java
@@ -0,0 +1,45 @@
+package scanner.model;
+
+
+import scanner.model.enums.RoleType;
+import scanner.model.enums.UserState;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @Column(name = "MODIFIED_AT")
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "LAST_LOGIN")
+    private LocalDateTime lastLogin;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "ROLE_TYPE")
+    private RoleType roleType;
+
+    @Column(name = "USER_STATE")
+    private UserState userState;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "contact")
+    private String contact;
+}
