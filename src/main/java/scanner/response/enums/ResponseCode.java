cb32e8dee5d20d24cdf32ab9d0b67c2bb5c439c6
Floodnut <gsniper777@gmail.com>
2023-01-11 16:00:24 +0900

    feat: adjust Exception (#25)

--- src/main/java/scanner/response/enums/ResponseCode.java
+++ src/main/java/scanner/response/enums/ResponseCode.java
@@ -16,6 +16,7 @@ public enum ResponseCode {
     STATUS_4004(4004, HttpStatus.BAD_REQUEST,"Invalid File Format."),
     STATUS_4005(4005, HttpStatus.BAD_REQUEST,"Data missing."),
     STATUS_4007(4007, HttpStatus.BAD_REQUEST,"Invalid Request."),
+    STATUS_4008(4008, HttpStatus.BAD_REQUEST,"Invalid User."),
     /* Server Error. */
     STATUS_5001(5001, HttpStatus.INTERNAL_SERVER_ERROR,"Server busy."),
     STATUS_5002(5002, HttpStatus.INTERNAL_SERVER_ERROR,"Scan Error."),
