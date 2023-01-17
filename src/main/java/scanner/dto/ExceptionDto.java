ed0a0bd71756aaa4c47238ed432fc3a584824f0c
Floodnut <gsniper777@gmail.com>
2023-01-17 15:18:30 +0900

    feat: adjust exception (#25)

--- src/main/java/scanner/dto/ExceptionDto.java
+++ src/main/java/scanner/dto/ExceptionDto.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 import scanner.response.enums.ResponseCode;
 
 @AllArgsConstructor
-@Builder
 @Getter
 public class ExceptionDto {
     private final int code;
     private final String message;
 
+    @Builder
     public ExceptionDto(ResponseCode res){
         this.code = res.getCode();
         this.message = res.getMessage();
