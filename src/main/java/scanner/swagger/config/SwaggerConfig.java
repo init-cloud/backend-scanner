ed5388109d67a69b1b3f188de6c4b0bae33ffc53
Floodnut <gsniper777@gmail.com>
2023-01-20 18:25:08 +0900

    fix: hot-fix for swagger build exception (#37)

--- src/main/java/scanner/swagger/config/SwaggerConfig.java
+++ src/main/java/scanner/swagger/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package scanner.swagger.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     private static final String API_NAME = "Init Cloud Scanner";
     private static final String API_VERSION = "2.0.0";
